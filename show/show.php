6837d3e7e750006deaf2ba2cc2a3b94dc5e58161
Marco <marco.carone@libero.it>
2020-03-04 18:27:23 +0100

    up

--- show/show.php
+++ show/show.php
@@ -23,7 +23,7 @@ include "server_show.php";
     </nav>
 
     <div class="row">
-      <div class="col-sm-12">
+      <div class="col-12">
         <div class="card">
           <div class="card-header">
             <h5 class="card-title">Dettagli della stanza</h5>
