0576e23b7eaf0d1c2386cb93a22e6fae9d65829b
Marco <marco.carone@libero.it>
2020-03-09 17:23:59 +0100

    AGGIUNTO PROTEZIONE  SQL Injections IN UPDATE

--- update/sql_update.php
+++ update/sql_update.php
@@ -2,16 +2,16 @@
   include __DIR__ . '/../database.php';
 
   if (empty($_POST['id'])) {
-    die('Id non inserito');
-}
+      die('Id non inserito');
+  }
   if (empty($_POST['room_number'])) {
-    die('No room number');
+      die('No room number');
   }
   if (empty($_POST['floor'])) {
-    die('No floor');
+      die('No floor');
   }
   if (empty($_POST['beds'])) {
-    die('No beds');
+      die('No beds');
   }
 
   $id_room = $_POST['id'];
@@ -21,7 +21,7 @@
 
   if ($result && $result->num_rows > 0) {
       $room = $result->fetch_assoc();
-      // var_dump($room);
+  // var_dump($room);
   } elseif ($result) {
       echo "id non esistente";
   } else {
@@ -32,21 +32,38 @@
 //se esiste quella stanza allora
 
 
+  // prepare and bind
+  $sql = "UPDATE `stanze` SET `room_number` = ?, `beds` = ?, `floor` = ? WHERE `id` = $id_room";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("iii", $roomNumber, $floor, $beds);
+  // set parameters and execute
   $roomNumber = $_POST['room_number'];
   $floor = $_POST['floor'];
   $beds = $_POST['beds'];
-  $sql = "UPDATE `stanze`
-  SET `room_number` = $roomNumber, `beds` = $floor, `floor` = $beds
-  WHERE `id` = $id_room";
 
-  $result = $conn->query($sql);
+  $stmt->execute();
 
-  if ($result) {
-    header("Location: $base_path/show/show.php?id=$id_room");
+  if ($stmt->affected_rows > 0) {
+      header("Location: $base_path/show/show.php?id=$id_room");
   } else {
-    echo 'KO';
+      echo 'KO';
   }
+  
+  // VERSIONE SENZA PREPARED STATEMENTS
+  // $roomNumber = $_POST['room_number'];
+  // $floor = $_POST['floor'];
+  // $beds = $_POST['beds'];
+  // $sql = "UPDATE `stanze`
+  // SET `room_number` = $roomNumber, `beds` = $floor, `floor` = $beds
+  // WHERE `id` = $id_room";
+  //
+  // $result = $conn->query($sql);
+  //
+  // if ($result) {
+  //     header("Location: $base_path/show/show.php?id=$id_room");
+  // } else {
+  //     echo 'KO';
+  // }
 
 
   $conn->close();
-?>
