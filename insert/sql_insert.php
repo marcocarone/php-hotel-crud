843b494ba0534c4165c4c5f96d2cdee461a7d034
Marco <marco.carone@libero.it>
2020-03-09 17:29:22 +0100

    AGGIUNTO PROTEZIONE  SQL Injections IN INSERT

--- insert/sql_insert.php
+++ insert/sql_insert.php
@@ -13,21 +13,41 @@
   }
 
 
+
+  // prepare and bind
+  $sql = "INSERT INTO `stanze` (room_number, floor, beds, created_at, updated_at)
+  VALUES (?, ?, ?, NOW(), NOW());";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("iii", $roomNumber, $floor, $beds);
+  // set parameters and execute
   $roomNumber = $_POST['room_number'];
   $floor = $_POST['floor'];
   $beds = $_POST['beds'];
 
+  $stmt->execute();
 
-  $sql = "INSERT INTO `stanze` (room_number, floor, beds, created_at, updated_at)
-  VALUES ($roomNumber, $floor, $beds, NOW(), NOW());";
-
-  $result = $conn->query($sql);
-
-  if ($result) {
-    header("Location: $base_path/index.php");
+  if ($stmt->affected_rows > 0) {
+      header("Location: $base_path/index.php");
   } else {
-    echo 'problema riscontrato con sql';
+      echo 'problema riscontrato con sql';
   }
+
+  // VERSIONE SENZA PREPARED STATEMENTS
+  // $roomNumber = $_POST['room_number'];
+  // $floor = $_POST['floor'];
+  // $beds = $_POST['beds'];
+  //
+  //
+  // $sql = "INSERT INTO `stanze` (room_number, floor, beds, created_at, updated_at)
+  // VALUES ($roomNumber, $floor, $beds, NOW(), NOW());";
+  //
+  // $result = $conn->query($sql);
+  //
+  // if ($result) {
+  //   header("Location: $base_path/index.php");
+  // } else {
+  //   echo 'problema riscontrato con sql';
+  // }
 
 
   $conn->close();
